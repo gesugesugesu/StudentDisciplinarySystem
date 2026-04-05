8a20a4787b24fc24fd80a0747de84c74ff74bf56
Ralph Georges Marciano <marcianoralph12@gmail.com>
2026-04-06 00:45:22 +0800

    CORS

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ const PORT = process.env.PORT || 5000;
 const { initializeDatabase } = require('./database/db');
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGIN || '*',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
